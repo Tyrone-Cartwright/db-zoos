2dd50e6b78e430c7d35a3e213d47e149ea38832c
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-15 17:24:04 -0500

    Added GET to project and also added bears table to database

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const knex = require("knex");
+
+const knexConfig = require("./knexfile.js");
 
 const server = express();
+
+// connect to database
+const db = knex(knexConfig.development);
 
 server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.get("/", (req, res) => {
+  res.send("api working");
+});
+
+server.get("/api/zoos", (req, res) => {
+  db("zoos")
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+server.get("/api/zoos/:id", (req, res) => {
+  db(zoos)
+    .where({ id: req.params.id })
+    .then(zoos => {
+      if (zoos) {
+        res.status(200).json(zoos);
+      } else {
+        res.status(404).json({ message: "Zoo not found" });
+      }
+    });
+});
+
+server.post("/api/zoos", (req, res) => {
+  db("zoos")
+    .insert(req.body)
+    .then(zoos => {
+      if (zoos) {
+        res.status(200).json(zoos);
+      } else {
+        res.status(200).json({ message: "Please enter a zoo" });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
